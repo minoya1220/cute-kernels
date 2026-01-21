3fd6396b79d763b58e8b5274036b421a9ca12118
minoya1220 <minoya1220@gmail.com>
2026-01-21 06:02:17 +0000

    tested cute dsl

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from cute-kernels!")
+print("file launched")
+import cutlass
+import cutlass.cute as cute
+print("imported")
 
+@cute.kernel
+def kernel():
+    tid, _, _ = cute.arch.thread_idx()
+    if tid == 0:
+        cute.printf("dwadowjad")
 
-if __name__ == "__main__":
-    main()
+@cute.jit
+def kernel_launcher():
+    cute.printf("launching kernel...")
+
+    kernel().launch(
+        grid=(1,1,1),
+        block=(1,1,1)
+    )
+print("func deffed")
+
+cutlass.cuda.initialize_cuda_context()
+print("context")
+
+print("running jit compiled")
+kernel_launcher()
+
+print("precompiled and objdump")
+kernel_compiled = cute.compile(kernel_launcher, options="") # options: --keep-ptx --keep-cubin 
+kernel_compiled()
