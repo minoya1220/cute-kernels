0c9ab2e4cb3e5a7e3228bb2166d80b7ebfc82cc9
minoya1220 <minoya1220@gmail.com>
2026-04-28 03:40:46 +0000

    unoptimized online safe softmax kernel finished

--- softmax.py
+++ softmax.py
@@ -1,4 +1,5 @@
 import cutlass
+import operator
 import torch
 import cutlass.cute as cute
 from cutlass import Int32, Int64, Float32, Boolean, const_expr
@@ -14,29 +15,57 @@ def softmax_fwd_kernel(gX: cute.Tensor, gY: cute.Tensor):
     bidx, _, _ = cute.arch.block_idx()
     bdim, _, _ = cute.arch.block_dim()
     widx, lidx = cute.arch.warp_idx(), cute.arch.lane_idx()
+    
+    smem = cutlass.utils.SmemAllocator()
+    buffer_layout = cute.make_layout(cute.arch.WARP_SIZE)
+    reduction_buffer = smem.allocate_tensor(cutlass.Float32, buffer_layout, byte_alignment=16)
+    max_buffer = smem.allocate_tensor(cutlass.Float32, buffer_layout, byte_alignment=16)
+    
+    # initialize SMEM buffers
+    if widx == 0:
+        reduction_buffer[lidx] = 0.0
+        max_buffer[lidx] = -Float32.inf
+    cute.arch.sync_threads()
 
     M, N = gX.shape
 
     m = bidx
-    n_iters = N // bdim
-    n = tidx
+    n_iters = cute.ceil_div(N, bdim)
 
     # single pass online safe softmax
     accum = 0.0
-    max = -Float32.inf
+    thread_max = -Float32.inf
     for ni in range(0, n_iters):
         # load and do the reduction
-        prev_max = max
-        elem = gX[bidx, n_iters * bdim + tidx]
-        max = cute.math.max(elem, max)
-        accum = accum * cute.math.exp(prev_max - max) + cute.math.exp(elem - max)
+        prev_max = thread_max
+        elem = gX[bidx, ni * bdim + tidx] if ni * bdim + tidx < N else -Float32.inf
+        thread_max = cute.arch.fmax(elem, thread_max)
+        accum = accum * cute.math.exp(prev_max - thread_max) + cute.math.exp(elem - thread_max)
 
-    # TODO: write reduction at block scale, use butterfly reduction
+    # intra-warp reduction
+    warp_max = cute.arch.warp_reduction(thread_max, cute.arch.fmax)
+    accum = accum * cute.math.exp(thread_max - warp_max)
+    accum = cute.arch.warp_reduction(accum, operator.add)
+    
+    # write each warp result to SMEM
+    if lidx == 0:
+        reduction_buffer[widx] = accum
+        max_buffer[widx] = warp_max
+    cute.arch.sync_threads()
 
-    # TODO: write elementwise finisher for softmax
+    # inter-warp reduction
+    partial_max = max_buffer[lidx] 
+    partial_result = reduction_buffer[lidx] 
+    full_max = cute.arch.warp_reduction(partial_max, cute.arch.fmax)
+    partial_result = partial_result * cute.math.exp(partial_max - full_max)
+    divisor = cute.arch.warp_reduction(partial_result, operator.add)
 
-
-    pass
+    # elementwise finisher
+    for ni in range(0, n_iters):
+        idx = ni * bdim + tidx
+        if idx < N:
+            elem = gX[bidx ,idx]
+            gY[bidx ,idx] = cute.math.exp(elem - full_max) / divisor
 
 
 @cute.jit
@@ -58,6 +87,6 @@ def softmax_bwd_launcher(T):
 
 if __name__ == "__main__":
     x = torch.randn(2**5)
-    torch_softmax = torch.nn.Softmax()
+    torch_softmax = torch.nn.Softmax(dim=0)
     print(x)
     print(torch_softmax(x, dim=0))
