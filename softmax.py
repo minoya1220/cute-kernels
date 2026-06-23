2c772b5ea32c638be1141ed8af5076b104bacabb
minoya1220 <minoya1220@gmail.com>
2026-06-22 17:51:55 -0700

    rewritten to be cute idiomatic and adding register caching for values

--- softmax.py
+++ softmax.py
@@ -10,11 +10,13 @@ from cutlass.cute.runtime import from_dlpack
 # assume f32 to start make more general later
 # gX and gY are 2D row-major tensors M x N where M is the batch dimension and N is the contiguous dim that softmax is applied to 
 # 
+BLOCK_SIZE = 256
+
 @cute.kernel
-def softmax_fwd_kernel(gX: cute.Tensor, gY: cute.Tensor):
+def softmax_fwd_kernel(gX: cute.Tensor, gY: cute.Tensor, cX: cute.Tensor, tv_layout: cute.Layout, tiler: cute.Shape, shape: cute.Shape):
     tidx, _, _ = cute.arch.thread_idx()
     bidx, _, _ = cute.arch.block_idx()
-    bdim, _, _ = cute.arch.block_dim()
+    bdim = BLOCK_SIZE # , _, _ = cute.arch.block_dim() # ensures that this value is known at compile time
     widx, lidx = cute.arch.warp_idx(), cute.arch.lane_idx()
     
     smem = cutlass.utils.SmemAllocator()
@@ -22,26 +24,43 @@ def softmax_fwd_kernel(gX: cute.Tensor, gY: cute.Tensor):
     reduction_buffer = smem.allocate_tensor(cutlass.Float32, buffer_layout, byte_alignment=16)
     max_buffer = smem.allocate_tensor(cutlass.Float32, buffer_layout, byte_alignment=16)
     
+    blk_coord = (bidx, None)
+    blkX = cute.local_tile(gX, tiler, blk_coord) # divides the block into tiles, boundary checks still have to be included
+    blkY = cute.local_tile(gY, tiler, blk_coord)
+    cblkX = cute.local_tile(cX, tiler, blk_coord)
+    
+    frgX = cute.composition(blkX, tv_layout)
+    frgY = cute.composition(blkY, tv_layout)
+    cfrgX = cute.composition(cblkX, tv_layout)
+
+    thr_coord = (tidx, None)
+    thrX = frgX[thr_coord]
+    thrY = frgY[thr_coord]
+    cthrX = cfrgX[thr_coord]
+
     # initialize SMEM buffers
     if widx == 0:
         reduction_buffer[lidx] = 0.0
         max_buffer[lidx] = -Float32.inf
     cute.arch.sync_threads()
 
-    M, N = gX.shape
+    M, N = shape
 
     m = bidx
     n_iters = cute.ceil_div(N, bdim)
+    rPred = cute.make_fragment(cthrX.shape, cutlass.Boolean)
+    rX = cute.make_rmem_tensor_like(thrX)
 
     # single pass online safe softmax
     accum = 0.0
     thread_max = -Float32.inf
     for ni in range(0, n_iters):
         # load and do the reduction
+        rPred[ni] = cute.elem_less(cthrX[ni], shape)
+        rX[ni] = thrX[ni].load() if rPred[ni] else -Float32.inf
         prev_max = thread_max
-        elem = gX[bidx, ni * bdim + tidx] if ni * bdim + tidx < N else -Float32.inf
-        thread_max = cute.arch.fmax(elem, thread_max)
-        accum = accum * cute.math.exp(prev_max - thread_max) + cute.math.exp(elem - thread_max)
+        thread_max = cute.arch.fmax(rX[ni], thread_max)
+        accum = accum * cute.math.exp(prev_max - thread_max) + cute.math.exp(rX[ni] - thread_max)
 
     # intra-warp reduction
     warp_max = cute.arch.warp_reduction(thread_max, cute.arch.fmax)
@@ -63,25 +82,35 @@ def softmax_fwd_kernel(gX: cute.Tensor, gY: cute.Tensor):
 
     # elementwise finisher
     for ni in range(0, n_iters):
-        idx = ni * bdim + tidx
-        if idx < N:
-            elem = gX[bidx ,idx]
-            gY[bidx ,idx] = cute.math.exp(elem - full_max) / divisor
+        if rPred[ni]:
+            elem = rX[ni]
+            thrY[ni] = cute.math.exp(elem - full_max) / divisor
 
 
 @cute.jit
 def softmax_fwd_launcher(mX, mY):
     # mX is the input tensor, mY is the output
-    block_size = 256
+    bdim = BLOCK_SIZE
+    shape = mX.shape
+    M, N = shape
 
+    n_iters = cute.ceil_div(N, bdim)
+
+    thr_layout = cute.make_layout((bdim,))
+    val_layout = cute.make_layout((n_iters,), (bdim,))
+    # use cute.recast_layout to make this work for arbitrary dtypes
+    tiler, tv_layout = cute.make_layout_tv(thr_layout, val_layout)
     
-    fwd_kernel = softmax_fwd_kernel(mX, mY)
+    gX = mX # cute.zipped_divide(mY, tiler=tiler)
+    gY = mY # cute.zipped_divide(mX, tiler=tiler)
+    cX = cute.make_identity_tensor(mX.shape) # cute.zipped_divide(cute.make_identity_tensor(mX.shape), tiler=tiler)
+
+    fwd_kernel = softmax_fwd_kernel(gX, gY, cX, tv_layout, tiler, shape)
     fwd_kernel.launch(
         grid=(cute.size(mX.shape[:-1]), 1, 1),
-        block=(block_size, 1, 1)
+        block=(cute.size(tv_layout, mode=[0]), 1, 1)
     )
 
-    pass
 
 @cute.kernel
 def softmax_bwd_kernel(gY: cute.Tensor, gdY: cute.Tensor, gdX: cute.Tensor):
